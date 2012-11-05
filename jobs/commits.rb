d568b2e3c0b7bb2777f929993266ee01163c8179
jbfink <john.fink@gmail.com>
2012-11-04 20:23:25 -0500

    Too tired right now, but I think I know how to solve this.

--- jobs/commits.rb
+++ jobs/commits.rb
@@ -1,11 +1,12 @@
 require 'github_api'
-
+require 'json'
+# note to self -- response *has* to be JSON'd. That's the problem -- so figure out how to stuff author and comment into a JSON - follow the twitter.erb to see how to do it.
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
 github = Github.new
 commits = github.repos.commits.all 'jbfink', 'tvpeople'
-lastmessage = commits[0].commit.message
+lastmessage = commits[0].commit.message.to_json
 
   send_event('commits',  comments: lastmessage )
 end
