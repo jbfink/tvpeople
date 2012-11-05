b3f08d41f9b2d47e9693294be26aa2f6b8a2476b
jbfink <john.fink@gmail.com>
2012-11-04 20:07:21 -0500

    Getting there.

--- jobs/commits.rb
+++ jobs/commits.rb
@@ -1,9 +1,11 @@
 require 'github_api'
 
-github = Github.new
-commits = github.repos.commits.all 'jbfink', 'tvpeople'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('commits',  commits[0].commit.message )
+github = Github.new
+commits = github.repos.commits.all 'jbfink', 'tvpeople'
+lastmessage = commits[0].commit.message
+
+  send_event('commits',  comments: lastmessage )
 end
